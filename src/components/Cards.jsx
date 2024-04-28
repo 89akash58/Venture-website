a923d4887ce24edd649d096ae46435569868a60d
akash.ssingh.aakkaa@gmail.com <akash.ssingh.aakkaa@gmail.com>
2024-04-28 13:54:02 +0530

    second commit

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -16,7 +16,7 @@ function Cards() {
               path="/services"
             />
             <CardItem
-              src="images/img-2.jpg"
+              src="./images/img-2.jpg"
               text="Travel through the Islands of Bali in a Private Cruise"
               label="Luxury"
               path="/services"
@@ -24,19 +24,19 @@ function Cards() {
           </ul>
           <ul className="cards__items">
             <CardItem
-              src="images/img-3.jpg"
+              src="./images/img-3.jpg"
               text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
               label="Mystery"
               path="/services"
             />
             <CardItem
-              src="images/img-5.jpg"
+              src="./images/img-5.jpg"
               text="The Places where cars live"
               label="Adventure"
               path="/products"
             />
             <CardItem
-              src="images/img-8.jpg"
+              src="./images/img-8.jpg"
               text="Ride through the Sahara Desert on a guided camel tour"
               label="Adrenaline"
               path="/sign-up"
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,7 +5,7 @@ import "./HeroSection.css";
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="/videos/video-1.mp4" autoPlay loop muted></video>
+      <video src="./videos/video-1.mp4" autoPlay loop muted></video>
       <h1>VENTURE AWAITS</h1>
       <p>Experience the future of travel.</p>
       <div className="hero-btns">
