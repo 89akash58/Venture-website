d1609b0479641eb4d4168ad7ebf107ecba8be2c6
akash.ssingh.aakkaa@gmail.com <akash.ssingh.aakkaa@gmail.com>
2024-05-02 02:03:40 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import Home from "./components/pages/Home";
 import Services from "./components/pages/Services";
 import Products from "./components/pages/Products";
 import SignUp from "./components/pages/SignUp";
-
+import NotFound from "./components/pages/NotFound";
 function App() {
   return (
     <>
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/Venture-website/" exact Component={Home} />
+          <Route path="/*" Component={NotFound} />
+          <Route path="/venture-website" Component={Home} />
+          <Route path="/" exact Component={Home} />
           <Route path="/services" Component={Services} />
           <Route path="/product" Component={Products} />
           <Route path="/sign-up" Component={SignUp} />
---
+++ src/components/pages/NotFound.jsx
@@ -0,0 +1,3 @@
+export default function NotFound() {
+  return <h1 style={{ textAlign: "center" }}>404 Error page not found</h1>;
+}
